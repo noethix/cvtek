7e6c8745b405db6b0f9d74925d932a6519a5c398
noethix <41691381+noethix@users.noreply.github.com>
2019-01-23 15:35:09 +0100

    Update cvtek4.php

--- cvtek4.php
+++ cvtek4.php
@@ -376,10 +376,28 @@ print "Nouveau Candidat" .PHP_EOL;
 print PHP_EOL;
 print("Nom: ");
 $informations["LastName"]=readline();
+while (empty($informations["LastName"]))
+{
+	print " CETTE INFORMATION EST OBLIGATOIRE POUR L'AJOUT DE TOUTE NOUVELLE CANDIDATURE.". PHP_EOL;
+	print("Nom: ");
+	$informations["LastName"]=readline();
+}	
 print("Prénom: ");
 $informations["FirstName"]=readline();
+while (empty($informations["FirstName"]))
+{
+	print " CETTE INFORMATION EST OBLIGATOIRE POUR L'AJOUT DE TOUTE NOUVELLE CANDIDATURE.". PHP_EOL;
+	print("Prénom: ");
+	$informations["FirstName"]=readline();
+}
 print("Date de naissance au format dd-mm-yyyy: ");
 $informations["Birthdate"]=readline();
+while (empty($informations["Birthdate"]))
+{
+	print " CETTE INFORMATION EST OBLIGATOIRE POUR L'AJOUT DE TOUTE NOUVELLE CANDIDATURE.". PHP_EOL;
+	print("Date de naissance au format dd-mm-yyyy: ");
+	$informations["Birthdate"]=readline();
+}
 $DOB= $informations["Birthdate"];
 print("Âge: ");
 print $informations["Age"]= age ($DOB) .PHP_EOL;
@@ -395,15 +413,47 @@ print("Téléphone fixe: ");
 $informations["Phone"]=readline();
 print("Téléphone portable: ");
 $informations["Mobile"]=readline ();
+while (empty($informations["Mobile"]))
+{
+	print " CETTE INFORMATION EST OBLIGATOIRE POUR L'AJOUT DE TOUTE NOUVELLE CANDIDATURE.". PHP_EOL;
+	print("Téléphone portable: ");
+	$informations["Mobile"]=readline ();
+}
 print("Mail: ");
 $informations["Mail"]= readline();
+while (empty($informations["Mail"]))
+{
+	print " CETTE INFORMATION EST OBLIGATOIRE POUR L'AJOUT DE TOUTE NOUVELLE CANDIDATURE.". PHP_EOL;
+	print("Mail: ");
+	$informations["Mail"]= readline();
+}
 print("Profil recherché: ");
 $informations["Profile"]=readline ();
-for($compteur;$compteur<10;$compteur++)
+while (empty($informations["Profile"]))
 {
-    print("Compétences: ");
-    $Skills[$compteur]=readline ();
+	print " CETTE INFORMATION EST OBLIGATOIRE POUR L'AJOUT DE TOUTE NOUVELLE CANDIDATURE.". PHP_EOL;
+	print("Profil recherché: ");
+	$informations["Profile"]=readline ();
+}	
+
+print("Compétences, de 5 minimum à 10 maximum: ") .PHP_EOL;
+for($i=0; $i < 10; $i++)
+{
+     $NumComp = $i+1;
+    print("Compétence n°".$NumComp) .PHP_EOL;
+    $buffer = readline();
+        if($i < 5 && empty($buffer))
+        {
+            while(empty($buffer))
+            {
+                print("Il y a moins de cinq compétences, merci de rentrer cinq compétences minimum") .PHP_EOL;
+                $buffer = readline();
+            }
+        }
+    $Skills[$i] = $buffer;
+
 }
+
 
 
 $informations["Skills"]=$Skills;
