fe13b73c21fd70801ab982d1c237b6b60679270f
noethix <41691381+noethix@users.noreply.github.com>
2019-03-07 08:55:51 +0100

    Add files via upload

---
+++ launchmenu.php
@@ -0,0 +1,73 @@
+<?php
+/*
+ *@Author : CAMPAGNE Cécile / SÉLIGNAC Marie / CARON Olivier
+ * HR TOOLS V 1.0
+ *
+ *
+ */
+
+// Variables
+$ID = 0;
+$IDs = 0;
+$saisie=0;
+$validation = false;
+$nul = "NULL";
+$Skills=array();
+$informations=array();
+$default_language = "EN.php";
+$compteur= 0;
+
+//DÉBUT DU PROGRAMME:
+la:
+print "Choisissez la langue/ Choose your language/ Escoge su idioma:". PHP_EOL;
+print "1 - Français". PHP_EOL;
+print "2 - English". PHP_EOL;
+print "3 - Español". PHP_EOL;
+$langue= readline ("Votre choix: ");
+// two switches are needed. First one sends you to MainMenu to get the chosen language, then comes back to the second one to send you to the actual main menu.
+switch ($langue)
+ {
+    case "1":
+    $default_language="FR.php";
+    break;
+  
+    case "2":
+    $default_language="EN.php";
+    break;
+  
+   case "3":
+    $default_language="ESP.php";
+    break;
+  
+ }
+
+//APPEL
+require_once('function.php');
+require_once('mainmenu.php');
+$candidats=csv_to_array('hrdata.csv');
+
+
+switch ($langue)
+ {
+    case "1":
+    MainMenu($candidats, $traduction);
+    break;
+  
+    case "2":
+    MainMenu($candidats, $traduction);
+    break;
+  
+    case "3":
+    MainMenu($candidats, $traduction);
+    break;
+ }
+
+
+
+
+
+
+
+
+
+?>
---
+++ mainmenu.php
@@ -0,0 +1,71 @@
+<?php
+//Call
+require_once('list.php');
+require_once('search.php');
+require_once('add.php');
+require_once('modification.php');
+require_once('translation'.$default_language);
+
+//Start
+//General menu:
+function MainMenu($candidats,$traduction)
+{
+
+    
+	$informations=array();
+    print PHP_EOL;
+    print $traduction["MAIN_MENU1"]. PHP_EOL;
+    print PHP_EOL;
+    print $traduction["MAIN_MENU2"]. PHP_EOL;
+    print $traduction["MAIN_MENU3"]. PHP_EOL;
+    print $traduction["MAIN_MENU4"]. PHP_EOL;
+    print $traduction["MAIN_MENU5"]. PHP_EOL;
+    print $traduction["MAIN_MENU6"]. PHP_EOL;
+    $menu=readline($traduction["MENU_CHOICE"]);
+    print  PHP_EOL;
+    switch ($menu) {
+        case "1":
+        li($candidats, $traduction);
+        MainMenu($candidats,$traduction);
+        case "2":
+        $candidats=add($candidats,$traduction);
+        MainMenu($candidats,$traduction);
+        case "3":
+        $candidats=modificationCandidats($candidats, $traduction);
+        MainMenu($candidats,$traduction);
+        case "4":   
+        search($candidats, $traduction);
+        MainMenu($candidats,$traduction);
+        case "5":
+        quit($candidats, $traduction);
+        case "6":
+        open_cv($candidats);
+        default:
+        print $traduction["MAIN_MENU_DEFAULT"]. PHP_EOL;
+        print PHP_EOL;
+        MainMenu($candidats,$traduction);
+
+    }
+}
+
+//End
+function quit($candidats, $traduction) 
+{
+
+    print PHP_EOL;
+    print $traduction["MENU_QUIT1"];
+    $YesNo = readline();
+    $YesNo = strtoupper($YesNo);
+        if($YesNo[0] == "Y")
+        {
+            write ($candidats);
+            print $traduction["MENU_QUIT2"]. PHP_EOL;
+            exit;
+        }
+        else
+        {
+            MainMenu($candidats, $traduction);
+        }
+    
+}
+?>
